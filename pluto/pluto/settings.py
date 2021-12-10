59499f6ded8305a286f3e720f5bbf4f2cc9ea24f
Matheus-mVilela <matheus_vilela@icloud.com>
2021-12-10 15:25:06 -0300

    endpoints

--- pluto/pluto/settings.py
+++ pluto/pluto/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "starfish"
+    "starfish",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- pluto/starfish/views.py
+++ pluto/starfish/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, decorators, response, status
+from starfish import models, serializers
 
-# Create your views here.
+
+class SuggestionViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.SuggestionSerializer
+    queryset = models.Suggestion.objects.all()
+
+    @decorators.action(
+        methods=["post"],
+        url_path="vote",
+        url_name="vote",
+        detail=False,
+    )
+    def vote(self, reqeust):
+        return response.Response(
+            {"messege": 'Vary good!".'},
+            status=status.HTTP_200_OK,
+        )
